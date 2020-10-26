cc276161e0e2f8faad957c6e9892337c0a5f261c
johnche <john20chen@gmail.com>
2020-10-26 21:27:16 +0100

    new ginormous dump

--- scripts/sprites.js
+++ scripts/sprites.js
@@ -4,8 +4,8 @@ const asyncForEach = async (l, cb) => {
 	}
 }
 
-const sleep = async (ms) => {
-	return await new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const leftPadZero = (num, length) => num.toString().padStart(length, '0');
@@ -18,16 +18,19 @@ const drawFrames = async (drawnImage, frames, delay=100) => {
 }
 
 const getFramePath = (frameType, i) => `../assets/frames/${frameType}/${leftPadZero(i, 3)}.png`;
-const fetchAllFrames = (frameType, numFrames) => [...Array(numFrames + 1).keys()].map(i => getFramePath(frameType, i));
+const fetchAllFrames = (frameType, numFrames) => [...Array(numFrames + 1).keys()]
+		.map(i => getFramePath(frameType, i));
 
-const scene1_santaPreStart = fetchAllFrames('scene1/pre_start_1', 4);
-const scene1_santaPostStart = fetchAllFrames('scene1/post_start_1', 87);
+const scene1_intro = fetchAllFrames('scene1/1_intro', 16);
+const scene1_ride = fetchAllFrames('scene1/2_ride', 4);
+const scene1_brake_Fg = fetchAllFrames('scene1/3_brake/foreground', 87);
+const scene1_brake_Bg = fetchAllFrames('scene1/3_brake/background', 87);
 
-const scene2_introLoop = fetchAllFrames('scene2/1_intro_loop', 23);
+const scene2_intro = fetchAllFrames('scene2/1_intro', 23);
 const scene2_arrival = fetchAllFrames('scene2/2_arrival', 119);
-const scene2_3_postArrivalBg = fetchAllFrames('scene2/3_post_arrival_bg', 11);
-const scene2_3_postArrivalMg = ['../assets/frames/scene2/3_post_arrival_mg/000.png'];
-const scene2_3_postArrivalFg = fetchAllFrames('scene2/3_post_arrival_fg', 14);
+const scene2_3_postArrivalFg = fetchAllFrames('scene2/3_post_arrival/foreground', 14);
+const scene2_3_postArrivalMg = fetchAllFrames('scene2/3_post_arrival/middleground', 0);
+const scene2_3_postArrivalBg = fetchAllFrames('scene2/3_post_arrival/background', 23);
 
 let start = false;
 
@@ -40,34 +43,76 @@ const draw = (context, options) => {
 	);
 }
 
-async function run(foreground, middleground, background) {
-	while (!start) {
-		await drawFrames(foreground.image, scene1_santaPreStart);
+const loopFrames = async (image, frames, signal) => {
+	// To keep reference to outside variable, wrap run boolean in object
+	while(signal.run) {
+		await drawFrames(image, frames);
 	}
-	await drawFrames(foreground.image, scene1_santaPostStart);
+}
+
+const messageBox = document.getElementById('typewriter');
+const typeDialog = async (message) => {
+	asyncForEach(message, async letter => {
+		messageBox.innerHTML += letter;
+		await sleep(65);
+	})
+}
+const endDialog = () => {
+	messageBox.innerHTML = '';
+}
+
+const audio = new Audio();
+audio.muted = true;
+audio.volume = 0.5;
+const audioPlay = (path) => {
+	if (audio.src !== path) {
+		audio.src = path;
+	}
+	audio.play();
+}
+
+async function run(foreground, middleground, background) {
+	await drawFrames(foreground.image, scene1_intro);
+	while (!start) {
+		audioPlay('../assets/audio/scene1/gallop.ogg');
+		await drawFrames(foreground.image, scene1_ride);
+	}
+	const brakeSignal = {run: true}
+	audioPlay('../assets/audio/scene1/brake.ogg');
+	loopFrames(background.image, scene1_brake_Bg, brakeSignal);
+	await drawFrames(foreground.image, scene1_brake_Fg);
+	brakeSignal.run = false
 
 	// Scene 2 begin
-	await asyncForEach(Array(5), async () => {
-		await drawFrames(foreground.image, scene2_introLoop);
+	await asyncForEach(Array(1), async () => {
+		audioPlay('../assets/audio/scene2/snore.ogg');
+		await drawFrames(foreground.image, scene2_intro);
 	});
+
+	typeDialog('The quick green fox jumped over the lazy brown dog');
+	audioPlay('../assets/audio/scene2/arrival.ogg');
 	await drawFrames(foreground.image, scene2_arrival);
-	await drawFrames(middleground.image, scene2_3_postArrivalMg);
-	while(true) {
-		await drawFrames(background.image, scene2_3_postArrivalBg);
-	}
+
+	const postArrivalSignal = {run: true};
+	drawFrames(middleground.image, scene2_3_postArrivalMg);
+	loopFrames(background.image, scene2_3_postArrivalBg, postArrivalSignal);
+	loopFrames(foreground.image, scene2_3_postArrivalFg, postArrivalSignal);
+	await sleep(100*1000);
+	postArrivalSignal.run = false;
 }
 
 function sprites() {
 	const startButton = document.getElementById('start-button');
 	startButton.onclick = () => {
 		start = true;
-		startButton.remove();
+		audio.muted = false; // bypass audioblock
+		startButton.style.display = 'none';
 	}
 
 	const [fg, mg, bg] = ['foregroundCanvas', 'middlegroundCanvas', 'backgroundCanvas'].map(
 		canvasId => {
 			const context = document.getElementById(canvasId).getContext('2d');
-			const imgData = { image: new Image, dx: 0, dy: 0 };
+			const imgData = { image: new Image, dx: 0, dy: 0, context };
 			imgData.image.onload = () => {
 				clearCanvas(context);
 				draw(context, imgData);
