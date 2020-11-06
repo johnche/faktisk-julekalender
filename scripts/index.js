7ded012c47504f45c76404336d7ef59042254ec9
pellepropell <19978264+pbarrattdue@users.noreply.github.com>
2020-11-06 14:19:37 -0800

    sfx for button and textbox, textbox not implemented yet

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import * as snow from './modules/snow.js';
 import * as audio from './modules/audio/index.js';
 import * as animation from './modules/animation/index.js'
+import { audioPlay } from './modules/audio/utils.js'
 
 function main() {
 	const audioConfig = {
@@ -15,6 +16,7 @@ function main() {
 	const startButton = document.getElementById('start-button');
 	startButton.onclick = () => {
 		audio.unmute(); // bypass audioblock
+		audioPlay('assets/audio/button.ogg', audio.getAudioChannel(2));
 		startButton.style.display = 'none';
 		animation.play();
 	}
