7f16c476108835668968697dcf831489e8ccaae6
johnche <john20chen@gmail.com>
2020-11-06 21:43:00 +0100

    add fast forward to dialog

--- scripts/modules/typography.js
+++ scripts/modules/typography.js
@@ -6,15 +6,23 @@ const messageBox = document.getElementById('text');
 const messageContent = document.getElementById('typewriter');
 
 export const typeDialog = async (message) => {
+	let impatient = false;
+	messageBox.onclick = () => impatient = true;
+
 	getAudioChannel(2).volume = 0.02;
 	messageBox.style.display = 'block';
 	await asyncForEach(message, async letter => {
+		if (impatient) {
+			messageContent.innerHTML = message;
+			return false;
+		}
 		messageContent.innerHTML += letter;
 		await audioPlay('assets/audio/text.ogg', getAudioChannel(2));
-		await sleep(20);
+		await sleep(40);
 	});
 
 	await waitForClick(messageBox);
 	messageBox.style.display = 'none';
 	messageContent.innerHTML = '';
+	messageBox.onclick = null;
 };
